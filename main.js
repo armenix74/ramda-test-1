939b5c7f2b6366549c1f2dc4e7121815d40bb582
sergio armenia <sergio.armenia@netsenseweb.com>
2020-03-11 08:49:35 +0100

    added StudendHavingACertainScore

--- main.js
+++ main.js
@@ -4,15 +4,12 @@ const fs = require('fs');
 let rawdata = fs.readFileSync('students.json');
 let students = JSON.parse(rawdata);
 const averageThreshold = 16;
+const certainScore = 30;
 
 const studentAvgScore = student => {
-    const studentScores = [];
-    student.results.map((elem, i) => {
-        studentScores.push(elem.score);
-    });
-    const len = R.length(studentScores);
-    const avg = R.sum(studentScores) / len;
-    return R.merge(student, { avgScore: avg });
+    const studentScores = R.map(r => r.score, student.results);
+    const avg = R.mean(studentScores);
+    return R.merge(student, { allScores: studentScores, avgScore: avg });
 }
 
 const filterByAvgScore = R.curry((threshold, student) => {
@@ -48,6 +45,26 @@ const bestScoredStudents = (students) => {
     )(students)
 }
 
+const filterScoreWithAValue = R.curry((value, student) => {
+    const studentScores = R.map(r => r.score, student.results);
+    return R.any(e => e == value)(studentScores);
+})
+
+
+const studentHavingAtLeastACertainScore = (value, students) => {
+    return R.filter(filterScoreWithAValue(value), students);
+}
+
+
+
+
+console.log('================================');
+console.log('PARAMETERS:');
+console.log('================================');
+console.log('averageThreshold: ', averageThreshold);
+console.log('certainScore: ', certainScore);
+
+
 
 console.log('================================');
 console.log('ALL SCORED STUDENTS');
@@ -55,7 +72,7 @@ console.log('================================');
 console.log('students list: ', scoredStudents(students));
 
 console.log('================================');
-console.log('STUDENTS WITH AVG SCORE GREATER THAN ', averageThreshold);
+console.log('STUDENTS WITH AVG SCORE GREATER THAN', averageThreshold);
 console.log('================================');
 
 const scope = studentsWithScoreAboveValuePiped(averageThreshold, students)
@@ -68,4 +85,8 @@ console.log('================================');
 console.log('The best student is: ')
 console.log(bestScoredStudents(students));
 
+console.log('================================');
+console.log('STUDENTS HAVING A CERTAIN SCORE of (', certainScore, ')');
+console.log('================================');
 
+console.log(studentHavingAtLeastACertainScore(certainScore, students));
